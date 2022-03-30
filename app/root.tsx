967b10271ec103fe078845a37950dff3924545dc
Tyler Termini <tntermini@gmail.com>
2022-03-29 20:35:55 -0700

    tailwind init

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "remix";
+import styles from "./styles/app.css"
 import type { MetaFunction } from "remix";
 
 export const meta: MetaFunction = () => ({
@@ -13,6 +14,10 @@ export const meta: MetaFunction = () => ({
   title: "termini.life",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }]
+}
 
 export default function App() {
   return (
