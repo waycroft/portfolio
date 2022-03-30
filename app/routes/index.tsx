ae46bd242d24e7a81a14eb024b0dd962cd476556
Tyler Termini <tntermini@gmail.com>
2022-03-30 15:13:03 -0700

    dummy text

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,35 @@
 export default function Index() {
   return (
-    <div className="prose font-mono">
-      <h1>Tyler Termini - full stack developer</h1>
+    <div className="font-mono p-8 border-[32px] border-primary h-screen">
+      <h1 className="text-6xl">Tyler Termini - full stack developer.</h1>
+      <p className="text-4xl">
+        {" "}
+        Phasellus vehicula velit ac est consectetur, vitae aliquet neque
+        commodo. Nullam eu lacus dictum lectus fermentum ultrices quis in erat.
+        Aliquam ac dolor nec nunc pretium pretium sit amet ac sem. Fusce
+        scelerisque metus non vehicula finibus. Ut auctor congue lectus, id
+        dictum turpis cursus quis. Nunc sed lobortis lacus, sit amet vestibulum
+        ipsum. Vestibulum cursus tortor non porttitor mollis. Nullam tincidunt
+        dolor eu fringilla gravida. Curabitur ac pulvinar nunc. Quisque quis
+        sagittis arcu. Maecenas pulvinar accumsan dui ac euismod. Ut malesuada
+        dignissim elementum.{" "}
+      </p>
+      <p>
+        {" "}
+        Donec congue turpis lorem, a suscipit sem mattis interdum. Sed quam
+        mauris, feugiat ac massa id, dignissim finibus tortor. Nullam nec libero
+        et felis porttitor maximus vitae ut nunc. Aliquam sagittis pharetra quam
+        vel interdum. Integer quam leo, vulputate et mattis nec, imperdiet nec
+        dui. Suspendisse et eros a tortor ultrices pellentesque non pharetra
+        tortor. Cras eu feugiat nisl. Morbi elementum scelerisque metus, sit
+        amet volutpat magna consequat sit amet. Proin nec lacus at arcu congue
+        pretium et eu massa. Donec ac eros faucibus, tempor magna vitae,
+        fringilla risus. Suspendisse pharetra mi ut nulla pellentesque luctus ac
+        id odio. Aenean magna diam, hendrerit nec lacus in, mollis interdum
+        metus. Etiam rhoncus sollicitudin blandit. Vestibulum a euismod lectus.
+        Aliquam tortor velit, scelerisque ac aliquet condimentum, congue ut
+        nulla.{" "}
+      </p>
     </div>
   );
 }
